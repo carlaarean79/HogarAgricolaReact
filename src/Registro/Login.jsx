18280baad93004aad2bee5ef39be721172063c82
carlaarean79 <carlaandreaarean@gmail.com>
2024-02-13 23:55:04 -0300

    login

--- src/Registro/Login.jsx
+++ src/Registro/Login.jsx
@@ -1,13 +1,52 @@
 import React, { useRef } from 'react';
 import Swal from 'sweetalert2';
+import Input from '../input/input';
+import './Login.css'
+import Button from '../Button/Button';
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
-  
+  const url_User = 'http://localhost:3030/users'
+
+  //solicitud al servidor para verificar credenciales del usuario
+  const handleLogin = async (e) =>{
+e.preventDefault();
+const email = e.target.email.value;
+const contraseña = e.target.contraseña.value;
+const navigate = useNavigate();
+try{
+
+if(Response.ok) {
+  Swal.fire('¡Inicio de sesión exitoso!','Bienvenido de nuevo', 'success');
+  navigate('/cuenta');
+} else {
+  Swal.fire('Error','Ha ocurrido un error al intentar iniciar sesión. Intente nuevamente','error')
+}
+
+
+} catch (error){
+
+}//fin catch
+
+
+  }
 
   return (
-    <div>
+    <>
+    <h1 className='login-h1'>LOGIN</h1>
+    <form className='container-login-all'>
       
-    </div>
+      <Input className={'input-login'} label={'Email'} placeholder={'Email'} type={'email'} name={'email'} /* value={user_logged} *//>
+      <Input className={'input-login'} label={'Contraseña'} placeholder={'Contraseña'} type={'password'} name={'contraseña'} /* value={user_logged} *//>
+     
+   <div className='container-btns'>
+    <Button className={'btns-submit'} type={'submit'} title={'ENVIAR'}/>
+    <Button className={'btns-cancel'} type={'reset'} title={'CANCELAR'}/>
+   </div>
+    </form>
+    
+    </>
   );
 };
 
--- src/input/input.jsx
+++ src/input/input.jsx
@@ -5,7 +5,7 @@ const Input = ({label,className,placeholder,type,name,value,onChange, onKeyDown}
     <div>
         <div className={className}>
             <label>{label}</label>
-            <input className={className} placeholder= {placeholder} type={type} name={name} value={value} onChange={onChange} onKeyDown={onKeyDown}/>
+            <input placeholder= {placeholder} type={type} name={name} value={value} onChange={onChange} onKeyDown={onKeyDown}/>
         </div>
     </div>
   )
